c682700ecda74e2fd3214fbe7225f1c0233dda23
mike <me.menge47@gmail.com>
2022-11-30 18:35:01 -0600

    edit q# 1 format

--- exercise/LoopExcersice.java
+++ exercise/LoopExcersice.java
@@ -19,13 +19,14 @@ public class LoopExcersice {
 	}// main
 
 	private void question_1() {
+	
 		System.out.println("\nAnswer for # 1");
 
 		for (int i = 1; i <= 12; i++) {
 
 			for (int j = 1; j <= 12; j++) {
-
-				System.out.print(i * j + "  ");
+			
+			 System.out.printf("%-5d", i*j );
 			}
 
 			System.out.println(" ");
